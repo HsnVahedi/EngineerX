44b888aea5ab522406890fa499971a494edd831f
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-29 14:01:03 +0430

    d

--- backend/part-1/engineerx/engineerx/settings/production.py
+++ backend/part-1/engineerx/engineerx/settings/production.py
@@ -2,6 +2,17 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
 try:
     from .local import *
 except ImportError:
--- backend/part-1/engineerx/start.sh
+++ backend/part-1/engineerx/start.sh
@@ -1,7 +1,8 @@
-rm manage.py && mv manage.local.py manage.py
-rm engineerx/wsgi.py && mv engineerx/wsgi.local.py engineerx/wsgi.py
-mv -vn downloads/ media/downloads/
+rm manage.py && mv manage.production.py manage.py
+rm engineerx/wsgi.py && mv engineerx/wsgi.production.py engineerx/wsgi.py
+
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
 gunicorn engineerx.wsgi:application
