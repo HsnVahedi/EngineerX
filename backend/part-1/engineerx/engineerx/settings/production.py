1f98c37108a7ac470af34124368b557f20a5205f
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-29 19:02:53 +0430

    d

--- backend/part-1/engineerx/engineerx/settings/production.py
+++ backend/part-1/engineerx/engineerx/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
